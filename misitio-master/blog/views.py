5da863ad6f25d8b8a453701b834888b6c4f2c92a
ryacyna <ryacyna@gmail.com>
2018-09-06 19:42:45 -0300

    modifique error en palabra costo

---
+++ misitio-master/blog/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
